8518ff186609e52c2c1c94081ca46c0d6017c2e4
Axolotl <samuelmachat@gmail.com>
2019-01-08 17:49:54 -0500

    create migration for users

--- database/migrations/20190108172400_users.js
+++ database/migrations/20190108172400_users.js
@@ -1,12 +1,15 @@
-exports.up = function(knex, Promise) {}
+exports.up = knex =>
+  knex.schema.createTable('users', table => {
+    table.uuid('id').primary()
 
-exports.down = function(knex, Promise) {}
+    table.string('first_name', 128).notNullable()
+    table.string('last_name', 128).notNullable()
 
-// User
-// id
-// role (owner, supervisor, employee)
-// First name
-// Last name
-// Password
-// email
-// phone
+    table.string('role', 128).notNullable()
+    // table.string('password').notNullable() with oauth do we need password in db?
+
+    table.string('email')
+    table.string('phone')
+  })
+
+exports.down = knex => knex.schema.dropTableIfExists('users')
