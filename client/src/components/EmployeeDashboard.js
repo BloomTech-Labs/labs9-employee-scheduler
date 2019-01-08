ffa6b185fff34941089bcde40bb04e89cc0a8e4c
Carlos Lantigua <clantigua954@gmail.com>
2019-01-08 18:40:45 -0500

    adding employeedashboard component

---
+++ client/src/components/EmployeeDashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// This page will house all of the information that will be visible to the employees when they log in to the site
+class EmployeeDashboard extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Employee Dashboard</h1>
+			</div>
+		);
+	}
+}
+
+export default EmployeeDashboard;
