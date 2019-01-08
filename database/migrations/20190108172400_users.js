1b663a0b5d513cd74a6eabecc9c22548766667d2
Axolotl <samuelmachat@gmail.com>
2019-01-08 18:22:46 -0500

    create migration for availabilities

--- database/migrations/20190108172400_users.js
+++ database/migrations/20190108172400_users.js
@@ -1,13 +1,9 @@
 exports.up = knex =>
   knex.schema.createTable('users', table => {
     table.uuid('id').primary()
-
     table.string('first_name', 128).notNullable()
     table.string('last_name', 128).notNullable()
-
     table.string('role', 128).notNullable()
-    // table.string('password').notNullable() with oauth do we need password in db?
-
     table.string('email')
     table.string('phone')
   })
--- database/migrations/20190108172418_availabilities.js
+++ database/migrations/20190108172418_availabilities.js
@@ -1,9 +1,17 @@
-exports.up = function(knex, Promise) {}
+exports.up = knex =>
+  knex.schema.createTable('availabilities', table => {
+    table
+      .uuid('id')
+      .primary()
+      .defaultTo(knex.raw('uuid_generate_v4()'))
+    table
+      .uuid('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+    table.date('day').notNullable()
+    table.time('start_time')
+    table.time('end_time')
+  })
 
-exports.down = function(knex, Promise) {}
-
-// Availability
-// user
-// day
-// start time
-// end time
+exports.down = knex => knex.schema.dropTableIfExists('availabilities')
