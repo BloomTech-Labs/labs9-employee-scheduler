4f0613a58439f8f6f07c52994bb9b4198d248235
Carlos Lantigua <clantigua954@gmail.com>
2019-01-08 18:25:20 -0500

    adding template components

---
+++ client/src/components/Calendar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// this component might feature a full monthly read only version of the calendar?
+class Calendar extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Calendar</h1>
+			</div>
+		);
+	}
+}
+
+export default Calendar;
---
+++ client/src/components/CreateSchedule.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// this component will house all of the main features for the create schedule page.
+class CreateSchedule extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Create Schedule</h1>
+			</div>
+		);
+	}
+}
+
+export default CreateSchedule;
