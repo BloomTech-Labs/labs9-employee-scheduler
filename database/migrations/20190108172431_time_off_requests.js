3d6c75f09a949be54421d1f43d250baae14090b0
Axolotl <samuelmachat@gmail.com>
2019-01-08 18:30:10 -0500

    create migration for time_off_requests

--- database/migrations/20190108172431_time_off_requests.js
+++ database/migrations/20190108172431_time_off_requests.js
@@ -1,9 +1,17 @@
-exports.up = function(knex, Promise) {}
+exports.up = knex =>
+  knex.schema.createTable('time_off_requests', table => {
+    table
+      .uuid('id')
+      .primary()
+      .defaultTo(knex.raw('uuid_generate_v4()'))
+    table
+      .uuid('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+    table.date('date').notNullable()
+    table.string('reason', 1024)
+    table.string('status').defaultTo('pending')
+  })
 
-exports.down = function(knex, Promise) {}
-
-// Time off requests
-// date
-// user
-// reason
-// confirmed/pending/denied
+exports.down = knex => knex.schema.dropTableIfExists('time_off_requests')
