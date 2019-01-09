2f8a5c9e80fdcd89d0409ab63fa22cf0597f98dc
Kamry Bowman <kamry.bowman@gmail.com>
2019-01-09 10:04:23 -0700

    implement first router test

--- client/src/__tests__/App.js
+++ client/src/__tests__/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from '../App'
 
-it('renders hello', () => {
-  const { getByText } = render(<App />)
-  expect(getByText('hello')).toBeInTheDocument()
+describe('App component', () => {
+  it('renders hello', () => {
+    const { getByText } = render(
+      <Router>
+        <App />
+      </Router>
+    )
+    expect(getByText('hello')).toBeInTheDocument()
+  })
+  it('routes to calendar', () => {
+    const { getByText, container } = render(
+      <Router>
+        <App />
+      </Router>
+    )
+    const leftClick = { button: 0 }
+    fireEvent.click(getByText(/calendar/i), leftClick)
+    expect(container.innerHTML).toMatch('Calendar')
+  })
 })
