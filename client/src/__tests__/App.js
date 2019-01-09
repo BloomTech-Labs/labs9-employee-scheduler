b47e5ae6f5fa8d3fc38ddddaedd887c4d8822ad8
adamhinckley <adamhinckley@mac.com>
2019-01-08 23:08:38 -0700

    adds component for firebase authentication and cleanup on tests

--- client/src/__tests__/App.js
+++ client/src/__tests__/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import { render, cleanup } from 'react-testing-library'
 import App from '../App'
+
+afterEach(cleanup)
 
 it('renders hello', () => {
   const { getByText } = render(<App />)
