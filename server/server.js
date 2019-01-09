991deabbbfe08d652f3f49a53306082104237938
Rahul Desai <rd825@georgetown.edu>
2019-01-09 11:55:04 -0500

    Added router for users with GET functionality

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const configureMiddleware = require('./config/middleware.js')
 
 const server = express()
+const router = require('./routers/router')
 
 configureMiddleware(server)
 
 server.get('/', (req, res) => res.status(200).json({ message: 'hello!' }))
+
+server.use('/users', router)
 
 module.exports = server
