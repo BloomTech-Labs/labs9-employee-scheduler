4395524c76aef98de05be26288dccc053d8dce0c
Rahul Desai <rd825@georgetown.edu>
2019-01-10 14:01:16 -0500

    Added get, put, post routes for eventsRouter

--- server/routers/eventsRouter.js
+++ server/routers/eventsRouter.js
@@ -9,13 +9,39 @@ const {
 } = require('../../database/helpers')
 
 router.get('/', (req, res) => {
-  getUsers()
-    .then(users => res.status(200).json(users))
+  getEvents()
+    .then(events => res.status(200).json(events))
     .catch(err => res.status(404).json(err))
 })
 
-router.post('/', (req, res) => {})
-router.put('/', (req, res) => {})
-router.delete('/', (req, res) => {})
+router.post('/', async (req, res) => {
+  const { user_id, day, start_time, end_time } = req.body
+  if (!user_id || !day || !start_time || !end_time) {
+    res.status(400).json({ error: 'Missing required field(s)' })
+  }
+  try {
+    const success = await addEvent(req.body)
+    res.status(201).json(success)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Server error' })
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  if (!Object.keys(req.body)) {
+    res.status(400).json({ error: 'No fields provided to update' })
+  }
+  try {
+    const success = await updateEvent(id, req.body)
+    res.status(200).json(success)
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' })
+  }
+})
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+})
 
 module.exports = router
