c7d43045255652758c46c6b0b530a10cd5576ca3
Axolotl <samuelmachat@gmail.com>
2019-01-09 20:08:21 -0500

    first db helper (get orgs) working

--- database/data_model.md
+++ database/data_model.md
@@ -64,7 +64,7 @@
 
 `getOrgs()`
 
-`addOrg()`
+`addOrg(org)`
 
 `updateOrg(orgId)`
 
--- database/helpers/organizationsModel.js
+++ database/helpers/organizationsModel.js
@@ -1,7 +1,11 @@
 const db = require('../dbConfig')
 
-const getOrgs = async () => {}
-const addOrg = async () => {}
+const getOrgs = async () => {
+  const orgs = await db('organizations')
+  return orgs
+}
+const addOrg = async org => {}
+
 const updateOrg = async orgId => {}
 const deleteOrg = async orgId => {}
 
