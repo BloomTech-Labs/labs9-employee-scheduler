9053ab28a28412ff31b39f34cd0cf654617815ad
Kamry Bowman <kamry.bowman@gmail.com>
2019-01-10 14:20:47 -0700

    working through async timeout on test

--- database/utils/generateData.js
+++ database/utils/generateData.js
@@ -27,6 +27,26 @@ const generateUser = ({
   phone: faker.phone.phoneNumber()
 })
 
+const generateUsersForOrg = ({
+  org_id = uuid(),
+  quantity = generateRandomBetween(5, 50)
+}) => {
+  const users = []
+  for (let i = 0; i < quantity; i++) {
+    // set one owner, two supervisors, and the rest employees
+    const userRole = i === 0 ? 'owner' : i < 3 ? 'supervisor' : 'employee'
+
+    users.push(
+      generateUser({
+        user_id: uuid(),
+        org_id: org_id,
+        user_role: userRole
+      })
+    )
+  }
+  return users
+}
+
 const generateRandomBetween = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min
 
@@ -167,12 +187,55 @@ const generateDayOffRequests = (userId = uuid()) => {
   return requests
 }
 
+// Creates an object with values for all tables for one organization
+const populateOrg = (orgId = uuid()) => {
+  const organization = generateOrg(orgId)
+
+  const users = generateUsersForOrg({ org_id: organization.id })
+
+  let availabilities = []
+  let events = []
+  let timeOffRequests = []
+
+  users.forEach(user => {
+    availabilities = [...availabilities, ...generateAvailabilities(user.id)]
+    events = [...events, ...generateEvents(user.id)]
+    timeOffRequests = [...timeOffRequests, ...generateDayOffRequests(user.id)]
+  })
+
+  return { organization, users, availabilities, events, timeOffRequests }
+}
+
+// Takes an object with properties for each table for a single organization and inserts into a database
+const insertOrg = (
+  { organization, users, availabilities, events, timeOffRequests },
+  knex
+) => {
+  return knex.transaction(async function(trx) {
+    try {
+      await knex('organizations').insert(organization)
+      await knex('users').insert(users)
+
+      const availPromise = knex('availabilities').insert(availabilities)
+      const eventsPromise = knex('events').insert(events)
+      const timeOffPromise = knex('time_off_requests').insert(timeOffRequests)
+      await Promise.all([availPromise, eventsPromise, timeOffPromise])
+      return trx.commit()
+    } catch (err) {
+      return trx.abort()
+    }
+  })
+}
+
 module.exports = {
   generateOrg,
   generateOrgs,
   generateUser,
+  generateUsersForOrg,
   generateAvailabilities,
   generateDayOffRequest,
   generateDayOffRequests,
-  generateEvents
+  generateEvents,
+  populateOrg,
+  insertOrg
 }
