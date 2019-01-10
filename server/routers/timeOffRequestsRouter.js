2039e808cae9163020e792c444ddbac7ec5f3c6a
Axolotl <samuelmachat@gmail.com>
2019-01-09 22:58:49 -0500

    boilerplate all routers with correct db helper imports

--- server/routers/timeOffRequestsRouter.js
+++ server/routers/timeOffRequestsRouter.js
@@ -1,28 +1,21 @@
 const express = require('express')
 const router = express.Router()
-// const usersDb = require('../../database/helpers')
+const {
+  getTimeOffRequests,
+  addTimeOffRequest,
+  updateTimeOffRequest,
+  deleteTimeOffRequest,
+  getTimeOffRequestsForOrg
+} = require('../../database/helpers')
 
 router.get('/', (req, res) => {
-  // usersDb
-  //   .getUsers()
-  //   .then(users => res.status(200).json(users))
-  //   .catch(err => res.status(404).json(err))
+  getUsers()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(404).json(err))
 })
 
-router.post('/', (req, res) => {
-  // const { name } = req.body
-  // if (!name) {
-  //   res.send('Please enter a name')
-  // } else {
-  //   usersDb
-  //     .insert(name)
-  //     .then(count => res.status(201).json(count))
-  //     .catch(err => res.status(400).json(err))
-  // }
-})
-
-router.post('/current', (req, res) => {
-  return res.send(req.user.id)
-})
+router.post('/', (req, res) => {})
+router.put('/', (req, res) => {})
+router.delete('/', (req, res) => {})
 
 module.exports = router
