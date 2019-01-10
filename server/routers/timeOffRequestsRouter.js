2468ec5aaca90a90b4c80347546c2172f541b348
adamhinckley <adamhinckley@mac.com>
2019-01-10 13:57:02 -0700

    adds logic to timeOffRequests routes

--- server/routers/timeOffRequestsRouter.js
+++ server/routers/timeOffRequestsRouter.js
@@ -10,13 +10,49 @@ const {
 
 router.get('/', (req, res) => {
   getTimeOffRequests()
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(404).json(err))
+    .then(request => res.status(200).json(request))
+    .catch(err =>
+      res.status(404).json({ err: 'Error getting time off requests', err })
+    )
 })
 
-router.post('/', (req, res) => {})
+router.post('/', (req, res) => {
+  const { date, reason, status } = req.body
 
-router.put('/', (req, res) => {})
-router.delete('/', (req, res) => {})
+  if (!date || !reason || !status)
+    res.status(400).json({ error: 'Missing required field(s)', err })
+
+  addTimeOffRequest()
+    .then(request => res.status(200).json(request))
+    .catch(err => res.status(404).json({ err: 'Error with request', err }))
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params
+
+  if (!Object.keys(req.body)) {
+    res.status(400).json({ error: 'No fields provided to update', err })
+  }
+
+  updateTimeOffRequest(id)
+    .then(request => res.status(200).json(request))
+    .catch(err =>
+      res.status(404).json({ err: 'Error getting time off requests', err })
+    )
+})
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+  deleteTimeOffRequest(id)
+    .then(request => res.status(200).json(request))
+    .catch(err => res.status(404).json({ err: 'Error deleting request', err }))
+})
+
+router.get('/', (req, res) => {
+  getTimeOffRequestsForOrg()
+    .then(request => res.status(200).json(request))
+    .catch(err =>
+      res.status(404).json({ err: 'Error getting time off requests', err })
+    )
+})
 
 module.exports = router
