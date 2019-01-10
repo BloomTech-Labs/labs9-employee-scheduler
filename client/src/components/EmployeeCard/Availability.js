ade346b1f5ad4a8e27d1b540ac4ab1d47651bbc5
Axolotl <samuelmachat@gmail.com>
2019-01-10 13:12:41 -0500

    got components rendering with basic styling

--- client/src/components/EmployeeCard/Availability.js
+++ client/src/components/EmployeeCard/Availability.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import propTypes from 'prop-types'
+import styled from '@emotion/styled'
+import system from '../../design/theme'
 
 // this component should render the employee's weekly availability. It, in the future, will also have the ability to turn into a form to update such info.
 class Availability extends Component {
   render() {
     return (
-      <div>
+      <Container>
         {/* display the employee's weekly availability (e.g. Mon, weds. 8am to 5pm)
          in the employees directory, the supervisor should be able to select days and use a timepicker to alter this. */}
-      </div>
+        <p>availability info here</p>
+      </Container>
     )
   }
 }
@@ -18,3 +21,14 @@ export default Availability
 Availability.propTypes = {
   // adding propTypes here
 }
+
+const Container = styled('div')`
+  border: 1px solid grey;
+  padding: ${system.spacing.standardPadding};
+
+  p {
+    padding: ${system.spacing.standardPadding};
+    color: ${system.color.bodytext};
+    font-size: ${system.fontSizing.m};
+  }
+`
--- client/src/components/EmployeeCard/Card.js
+++ client/src/components/EmployeeCard/Card.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 import propTypes from 'prop-types'
+import styled from '@emotion/styled'
+import system from '../../design/theme'
+
+import Availability from './Availability'
+import TimeOff from './TimeOff'
 
 // this card component will contain the employee's info such as name, email, phone.
 // these cards will live in both the calendar page (view only) and the employees directory (edit possible)
 class Card extends Component {
   render() {
     return (
-      <div>
+      <Container>
         {/* Employee Name */}
+        <p>Name:</p>
+
         {/* Employee Email */}
+        <p>Email:</p>
+
         {/* Employee Phone */}
-      </div>
+        <p>Phone:</p>
+
+        <Availability />
+        <TimeOff />
+      </Container>
     )
   }
 }
@@ -20,3 +33,14 @@ export default Card
 Card.propTypes = {
   // adding propTypes here
 }
+
+const Container = styled('div')`
+  border: 1px solid grey;
+  padding: ${system.spacing.standardPadding};
+
+  p {
+    padding: ${system.spacing.standardPadding};
+    color: ${system.color.bodytext};
+    font-size: ${system.fontSizing.m};
+  }
+`
