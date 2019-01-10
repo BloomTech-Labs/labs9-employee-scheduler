4129c18ec13852c00a03ed3e0eaa5ce9a8139644
Rahul Desai <rd825@georgetown.edu>
2019-01-10 16:34:08 -0500

    Added events route for getEventsForOrg

--- server/routers/eventsRouter.js
+++ server/routers/eventsRouter.js
@@ -14,6 +14,12 @@ router.get('/', (req, res) => {
     .catch(err => res.status(404).json(err))
 })
 
+router.get('/all', (req, res) => {
+  getEventsForOrg()
+    .then(events => res.status(200).json(events))
+    .catch(err => res.status(404).json(err))
+})
+
 router.post('/', async (req, res) => {
   const { user_id, day, start_time, end_time } = req.body
   if (!user_id || !day || !start_time || !end_time) {
@@ -40,6 +46,7 @@ router.put('/:id', async (req, res) => {
     res.status(500).json({ error: 'Server error' })
   }
 })
+
 router.delete('/:id', async (req, res) => {
   const { id } = req.params
   try {
