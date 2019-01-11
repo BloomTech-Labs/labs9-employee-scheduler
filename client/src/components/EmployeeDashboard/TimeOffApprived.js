364c28423559c90ba433e7e6d3d18e3960491605
adamhinckley <adamhinckley@mac.com>
2019-01-11 11:33:02 -0700

    adds boilderplate for time off approved component

---
+++ client/src/components/EmployeeDashboard/TimeOffApprived.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TimeOffApproved extends Component {
+  render() {
+    return <div />
+  }
+}
+
+export default TimeOffApproved
+
+TimeOffApproved.propTypes = {
+  // adding propTypes here
+}
