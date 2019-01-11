c9e6f77d8f18cf406d765a738bb245c95aa17581
Axolotl <samuelmachat@gmail.com>
2019-01-11 16:30:40 -0500

    add comments

--- database/helpers/employeesHelper.js
+++ database/helpers/employeesHelper.js
@@ -1,8 +1,20 @@
 const db = require('../dbConfig')
 
 const getEmployees = async orgId => {
+  // For the get employees route, we need to return all users for a given org
+  // in addition, these employees also need to have the following info:
+  // availabilies and time off requests
+
+  // as there are three taables that we need to query and then crunch that data for
+  // this gets a bit complicated. In order to manage the complexity a little bit,
+  // I divided the query up into three, that then get compiled together.
+
+  // First, grab all the employees for a given org
   const employees = await db('users as u').where({ 'u.organization_id': orgId })
 
+  // Second, grab the availabilites for each employee and crunch the data together
+  // into an object of key value pairs where each key is a user id and the value
+  // is an array of the availabilies for that user
   const availabilities = await db('users as u')
     .where({ 'u.organization_id': orgId })
     .join('availabilities as a', { 'u.id': 'a.user_id' })
@@ -26,6 +38,9 @@ const getEmployees = async orgId => {
       return acc
     }, {})
 
+  // Third, grab the time off requests for each employee. The data is crunched in
+  // the same way: key value pairs where each key is a user id and the value
+  // is an array of the time off requests for that user
   const timeOffRequests = await db('users as u')
     .where({ 'u.organization_id': orgId })
     .join('time_off_requests as tor', { 'u.id': 'tor.user_id' })
@@ -49,6 +64,7 @@ const getEmployees = async orgId => {
       return acc
     }, {})
 
+  // Fourth, map over the employees and add the availabilies and time off requests
   const combined = employees.map(employee => {
     const { id } = employee
 
