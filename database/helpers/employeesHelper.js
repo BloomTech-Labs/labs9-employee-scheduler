5e384cd624af7fe82dd147e73ac9c813c7f7cb1e
Axolotl <samuelmachat@gmail.com>
2019-01-11 16:13:12 -0500

    /employees route working correctly

--- database/helpers/employeesHelper.js
+++ database/helpers/employeesHelper.js
@@ -15,13 +15,7 @@ const getEmployees = async orgId => {
     )
     .reduce((acc, current) => {
       const { user_id, availability_id, day, start_time, end_time } = current
-      const newItem = {
-        user_id,
-        id: availability_id,
-        day,
-        start_time,
-        end_time
-      }
+      const newItem = { id: availability_id, day, start_time, end_time }
 
       if (acc[user_id]) {
         acc[user_id].push(newItem)
@@ -34,46 +28,36 @@ const getEmployees = async orgId => {
 
   const timeOffRequests = await db('users as u')
     .where({ 'u.organization_id': orgId })
-    .join('time_off_requests as t', { 'u.id': 't.user_id' })
-  // .select(
-  //   'u.id as user_id',
-  //   'tor.id as time_off_request_id',
-  //   'tor.date',
-  //   'tor.reason',
-  //   'tor.status'
-  // )
-  // .reduce((acc, current) => {
-  //   const { user_id, time_off_request_id, date, reason, status } = current
-  //   const newItem = { id: time_off_request_id, date, reason, status }
-  //   console.log(current)
+    .join('time_off_requests as tor', { 'u.id': 'tor.user_id' })
+    .select(
+      'u.id as user_id',
+      'tor.id as time_off_request_id',
+      'tor.date',
+      'tor.reason',
+      'tor.status'
+    )
+    .reduce((acc, current) => {
+      const { user_id, time_off_request_id, date, reason, status } = current
+      const newItem = { id: time_off_request_id, date, reason, status }
 
-  //   if (acc[user_id]) {
-  //     acc[user_id].push(newItem)
-  //   } else {
-  //     acc[user_id] = [newItem]
-  //   }
+      if (acc[user_id]) {
+        acc[user_id].push(newItem)
+      } else {
+        acc[user_id] = [newItem]
+      }
 
-  //   return acc
-  // }, {})
-
-  console.log('time off requests')
-  console.log(timeOffRequests)
-
-  // console.log('Employees:')
-  // console.log(employees)
-  // console.log(availabilities)
+      return acc
+    }, {})
 
   const combined = employees.map(employee => {
     const { id } = employee
 
-    if (availabilities[id]) {
-      return { ...employee, availabilities: [...availabilities[id]] }
-    } else {
-      return { ...employee, availabilities: [] }
+    return {
+      ...employee,
+      availabilities: availabilities[id] ? [...availabilities[id]] : [],
+      time_off_requests: timeOffRequests[id] ? [...timeOffRequests[id]] : []
     }
   })
-
-  // console.log(combined)
 
   return combined
 }
