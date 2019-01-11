afc4907c2a3af28e6d6ea313d4b701f94eaf107d
Carlos Lantigua <clantigua954@gmail.com>
2019-01-11 13:58:21 -0500

    adding state data to employee component for mock

--- client/src/components/EmployeeDashboard.js
+++ client/src/components/EmployeeDashboard.js
@@ -3,7 +3,39 @@ import propTypes from 'prop-types'
 import BreadCrumb from './BreadCrumb'
 import LeftSideBar from './LeftSideBar'
 // This page will house all of the information that will be visible to the employees when they log in to the site
+
+//
 class EmployeeDashboard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {
+        firstname: 'Ariel',
+        lastname: 'Smith',
+        assignedShift: [
+          {
+            date: 'July 18',
+            times: '10am-2pm'
+          },
+          {
+            date: 'July 20',
+            times: '10am-2pm'
+          },
+          {
+            date: 'July 21',
+            times: '10am-2pm'
+          }
+        ],
+        timeOfApproved: {
+          date: 'July 24th'
+        },
+        timeOfRequest: {
+          data: 'July 25th',
+          reason: 'Sick day'
+        }
+      }
+    }
+  }
   render() {
     return (
       <div>
