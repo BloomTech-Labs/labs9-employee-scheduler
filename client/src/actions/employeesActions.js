0fd6d57d46f34f6175e1a82e9bea30b1baa4d8d5
Carlos Lantigua <clantigua954@gmail.com>
2019-01-11 20:48:40 -0500

    created redux action for fetching a single user

--- client/src/actions/employeesActions.js
+++ client/src/actions/employeesActions.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 export const FETCH_EMPLOYEES_FROM_DB_SUCCESS = 'FETCH_EMPLOYEES_FROM_DB_SUCCESS'
 export const FETCH_EMPLOYEES_FROM_DB_FAIL = 'FETCH_EMPLOYEES_FROM_DB_FAIL'
+export const FETCH_EMPLOYEE_FROM_DB_SUCCESS = 'FETCH_EMPLOYEE_FROM_DB_SUCCESS'
+export const FETCH_EMPLOYEE_FROM_DB_FAIL = 'FETCH_EMPLOYEE_FROM_DB_FAIL'
 
 const baseURL = process.env.REACT_APP_SERVER_URL
 
@@ -32,4 +34,23 @@ export const fetchEmployeesFromDB = orgId => dispatch => {
         dispatch({ type: FETCH_EMPLOYEES_FROM_DB_FAIL })
       })
   }
+}
+
+// fetches a single employee by user id
+export const fetchSingleEmployeeFromDB = userid => dispatch => {
+  axios
+    .get(`${baseURL}/dashboard/:id`, {
+      headers: { authorization: 'testing' }
+    })
+    .then(res =>
+      dispatch({
+        type: FETCH_EMPLOYEE_FROM_DB_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch(error =>
+      dispatch({
+        type: FETCH_EMPLOYEE_FROM_DB_FAIL
+      })
+    )
 }
--- client/src/reducers/employeesReducer.js
+++ client/src/reducers/employeesReducer.js
@@ -1,10 +1,13 @@
 import {
   FETCH_EMPLOYEES_FROM_DB_SUCCESS,
-  FETCH_EMPLOYEES_FROM_DB_FAIL
+  FETCH_EMPLOYEES_FROM_DB_FAIL,
+  FETCH_EMPLOYEE_FROM_DB_SUCCESS,
+  FETCH_EMPLOYEE_FROM_DB_FAIL
 } from '../actions'
 
 const initialState = {
   employees: [],
+  employee: [],
   error: ''
 }
 
@@ -13,6 +16,10 @@ export const employeesReducer = (state = initialState, action) => {
     case FETCH_EMPLOYEES_FROM_DB_SUCCESS:
       return { employees: [...action.payload], error: '' }
     case FETCH_EMPLOYEES_FROM_DB_FAIL:
+      return { ...initialState, error: 'fetching failed' }
+    case FETCH_EMPLOYEE_FROM_DB_SUCCESS:
+      return { employee: [...action.payload], error: '' }
+    case FETCH_EMPLOYEE_FROM_DB_FAIL:
       return { ...initialState, error: 'fetching failed' }
     default:
       return state
