e41355f8a3d9bca5405ee2f95f22ee08887e1947
Axolotl <samuelmachat@gmail.com>
2019-01-15 12:31:18 -0500

    rest of register owner form displaying

--- client/src/components/RegisterOwner.js
+++ client/src/components/RegisterOwner.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import styled from '@emotion/styled'
+import propTypes from 'prop-types'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 import firebase from 'firebase/app'
 // this import style is required for proper codesplitting of firebase
@@ -6,6 +8,10 @@ import 'firebase/auth'
 
 import { registerOwner } from '../actions' // for calling once all data is in
 import { connect } from 'react-redux'
+import Form from './Form/index'
+import BreadCrumb from './BreadCrumb'
+import SelectList from './common/SelectList'
+import system from '../design/theme'
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -38,8 +44,15 @@ const uiConfig = {
 }
 
 class RegisterOwner extends Component {
-  state = {}
-
+  state = {
+    oauthSuccess: false,
+    email: '',
+    phone: '',
+    firstName: '',
+    lastName: '',
+    orgName: '',
+    orgDescription: ''
+  }
   // check that user is not already logged in?
   // render form with info
   // stage 1
@@ -49,10 +62,15 @@ class RegisterOwner extends Component {
 
   componentDidMount() {
     this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
-      //checks to see if there is a user logged in.
-      const { currentUser } = firebase.auth()
-      console.log('CURRENT USER')
-      console.log(currentUser)
+      //checks to see if user has registered yet
+      if (firebase.auth().currentUser) {
+        const { email, displayName } = firebase.auth().currentUser
+        // In case user enters more than two names
+        const firstName = displayName.split(' ')[0]
+        const lastName = displayName.split(' ').slice(1)
+
+        this.setState({ email, firstName, lastName, oauthSuccess: true })
+      }
     })
   }
   // Make sure we un-register Firebase observers when the component unmounts.
@@ -60,12 +78,118 @@ class RegisterOwner extends Component {
     this.unregisterAuthObserver()
   }
 
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log('clicked')
+  }
+
   render() {
-    return false ? (
-      <h1>display this by default</h1>
-    ) : (
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-    )
+    const {
+      oauthSuccess,
+      email,
+      phone,
+      firstName,
+      lastName,
+      orgName,
+      orgDescription
+    } = this.state
+    const { handleChange, handleSubmit } = this
+
+    console.log(this.state)
+
+    if (!oauthSuccess) {
+      return (
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      )
+    } else {
+      return (
+        <Container>
+          <BreadCrumb />
+          <div className="wrapper">
+            <h1 className="headerText" data-testid="register-form">
+              Complete Registration
+            </h1>
+            <form type="submit" onClick={handleSubmit}>
+              <Form.Group
+                property="firstName"
+                type="text"
+                value={firstName}
+                handleChange={handleChange}
+                placeholder="First Name..."
+                ariaLabel="first-name"
+              >
+                <Form.Label>First Name</Form.Label>
+                <Form.TextInput />
+              </Form.Group>
+
+              <Form.Group
+                property="lastName"
+                type="text"
+                value={lastName}
+                handleChange={handleChange}
+                ariaLabel="last-name"
+              >
+                <Form.Label>Last Name</Form.Label>
+                <Form.TextInput />
+              </Form.Group>
+
+              <Form.Group
+                property="email"
+                type="text"
+                value={email}
+                handleChange={handleChange}
+                ariaLabel="email"
+              >
+                <Form.Label>Email</Form.Label>
+                <Form.TextInput />
+              </Form.Group>
+
+              <Form.Group
+                property="phone"
+                type="number"
+                value={phone}
+                handleChange={handleChange}
+                ariaLabel="phone"
+              >
+                <Form.Label>Phone Number</Form.Label>
+                <Form.TextInput />
+              </Form.Group>
+
+              <Form.Group
+                property="orgName"
+                type="text"
+                value={orgName}
+                handleChange={handleChange}
+                ariaLabel="org-name"
+              >
+                <Form.Label>Organizatoin Name</Form.Label>
+                <Form.TextInput />
+              </Form.Group>
+
+              <Form.Group
+                property="orgDescription"
+                type="text"
+                value={orgDescription}
+                handleChange={handleChange}
+                ariaLabel="org-description"
+              >
+                <Form.Label>Organization Description</Form.Label>
+                <Form.TextInput />
+              </Form.Group>
+
+              <button className="register">Register Organization</button>
+            </form>
+          </div>
+        </Container>
+      )
+    }
   }
 }
 
@@ -73,6 +197,70 @@ class RegisterOwner extends Component {
 const mapStateToProps = state => {
   return {}
 }
+
+const Container = styled('div')`
+  width: 100%;
+  min-width: 500px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  .wrapper {
+    margin: 30px auto 0 auto;
+    width: 100%;
+    max-width: 900px;
+    min-width: 500px;
+    padding: ${system.spacing.container};
+    .headerText {
+      font-size: ${system.fontSizing.l};
+      margin-bottom: 30px;
+    }
+    form {
+      width: 100%;
+      .form-group {
+        .form-label {
+          text-transform: uppercase;
+          font-weight: bold;
+          font-size: ${system.fontSizing.s};
+        }
+      }
+      .drop-down {
+        display: flex;
+        flex-direction: column;
+        height: 200px;
+        label {
+          text-transform: uppercase;
+          font-weight: bold;
+          font-size: ${system.fontSizing.s};
+          margin: 20px 0 20px 0;
+        }
+        select {
+          font-size: ${system.fontSizing.m};
+          border: ${system.borders.transparent};
+          border-radius: ${system.borders.radius};
+          outline: none;
+          cursor: pointer;
+        }
+      }
+      .register {
+        justify-content: flex-end;
+        padding: ${system.spacing.standardPadding};
+        background-color: ${system.color.primary};
+        color: ${system.color.neutral};
+        box-shadow: ${system.shadows.button};
+        border-radius: ${system.borders.radius};
+        border: ${system.borders.transparent};
+        font-size: ${system.fontSizing.m};
+        letter-spacing: 1px;
+        cursor: pointer;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          box-shadow: ${system.shadows.buttonHoverLight};
+          background-color: ${system.color.hoverPrimary};
+        }
+      }
+    }
+  }
+`
 
 // do we need redux??
 export default connect(
