dfe61d0c38ad63a37c4e241e4604cb6ffef87f43
Axolotl <samuelmachat@gmail.com>
2019-01-15 12:41:14 -0500

    abstract register container to own component

--- client/src/components/RegisterOwner.js
+++ client/src/components/RegisterOwner.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import styled from '@emotion/styled'
 import propTypes from 'prop-types'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 import firebase from 'firebase/app'
@@ -10,8 +9,7 @@ import { registerOwner } from '../actions' // for calling once all data is in
 import { connect } from 'react-redux'
 import Form from './Form/index'
 import BreadCrumb from './BreadCrumb'
-import SelectList from './common/SelectList'
-import system from '../design/theme'
+import { Container } from './common/RegisterContainer'
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -197,70 +195,6 @@ class RegisterOwner extends Component {
 const mapStateToProps = state => {
   return {}
 }
-
-const Container = styled('div')`
-  width: 100%;
-  min-width: 500px;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  .wrapper {
-    margin: 30px auto 0 auto;
-    width: 100%;
-    max-width: 900px;
-    min-width: 500px;
-    padding: ${system.spacing.container};
-    .headerText {
-      font-size: ${system.fontSizing.l};
-      margin-bottom: 30px;
-    }
-    form {
-      width: 100%;
-      .form-group {
-        .form-label {
-          text-transform: uppercase;
-          font-weight: bold;
-          font-size: ${system.fontSizing.s};
-        }
-      }
-      .drop-down {
-        display: flex;
-        flex-direction: column;
-        height: 200px;
-        label {
-          text-transform: uppercase;
-          font-weight: bold;
-          font-size: ${system.fontSizing.s};
-          margin: 20px 0 20px 0;
-        }
-        select {
-          font-size: ${system.fontSizing.m};
-          border: ${system.borders.transparent};
-          border-radius: ${system.borders.radius};
-          outline: none;
-          cursor: pointer;
-        }
-      }
-      .register {
-        justify-content: flex-end;
-        padding: ${system.spacing.standardPadding};
-        background-color: ${system.color.primary};
-        color: ${system.color.neutral};
-        box-shadow: ${system.shadows.button};
-        border-radius: ${system.borders.radius};
-        border: ${system.borders.transparent};
-        font-size: ${system.fontSizing.m};
-        letter-spacing: 1px;
-        cursor: pointer;
-        transition: 0.3s ease-in-out;
-        &:hover {
-          box-shadow: ${system.shadows.buttonHoverLight};
-          background-color: ${system.color.hoverPrimary};
-        }
-      }
-    }
-  }
-`
 
 // do we need redux??
 export default connect(
