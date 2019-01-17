6a2bb069d8bbf7c1d6e2970857913b88880ed8d9
adamhinckley <adamhinckley@mac.com>
2019-01-16 23:14:25 -0700

    updates data model, adds seed file, updates hours of operation migration

--- database/migrations/20190116223230_hours_of_operation.js
+++ database/migrations/20190116223230_hours_of_operation.js
@@ -1,12 +1,15 @@
 exports.up = knex =>
   knex.schema.createTable('hours-of-operation', table => {
     table
-      .uuid('organization_id', 100)
+      .uuid('organization_id')
       .primary()
-      .notNullable()
-    table.string('day')
-    table.integer('open_time')
-    table.integer('close_time')
+      .references('id')
+      .inTable('organizations')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+    table.string('day').notNullable()
+    table.integer('open_time').notNullable()
+    table.integer('close_time').notNullable()
   })
 
 exports.down = knex => knex.schema.dropTableIfExists('hours-of-operation')
