f3e8b25de72dcf62fea7491d2ecb446e2eaf447a
adamhinckley <adamhinckley@mac.com>
2019-01-16 19:18:53 -0700

    resolves merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -160,14 +160,11 @@ class App extends Component {
                 component={Settings}
               />
               <Route path="/register" component={RegisterOwner} />
-<<<<<<< HEAD
               <Route path="/billing" component={Billing} />
               <Route path="/calendar" component={Calendar} />
               <Route path="/dashboard/:id" component={Dashboard} />
               <Route path="/settings" component={Settings} />
               <Route path="/hours-of-operation" component={HoursOfOperation} />
-=======
->>>>>>> master
               <Route path="/login" render={props => <Login {...props} />} />
               <Route path="*" exact={true} component={FourOhFour} />
             </Switch>
