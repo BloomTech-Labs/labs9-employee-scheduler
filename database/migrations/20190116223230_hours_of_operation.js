b86f956eae155e6804c2e9cfa1d860a61bfac826
adamhinckley <adamhinckley@mac.com>
2019-01-18 12:54:41 -0700

    adds default times for hours of operation

--- database/migrations/20190116223230_hours_of_operation.js
+++ database/migrations/20190116223230_hours_of_operation.js
@@ -1,10 +1,15 @@
-//There is way more tables than there needs to be, it was made like this so I could use the seed for orgs.
 exports.up = knex =>
   knex.schema.createTable('hours_of_operation', table => {
     table.uuid('id').primary()
-    table.float('day') //should be notNullable but the seed is missing this for now
-    table.float('open_time') //should be notNullable but the seed is missing this for now
-    table.float('close_time') //should be notNullable but the seed is missing this for now
+    table.integer('day').notNullable()
+    table
+      .float('open_time')
+      .notNullable()
+      .defaultTo(8)
+    table
+      .float('close_time')
+      .notNullable()
+      .defaultTo(16)
     table.boolean('closed')
     table
       .uuid('organization_id')
