55afbbf9f1c7f2d491d2bd028bebfcd9ebf8d775
Rahul Desai <rd825@georgetown.edu>
2019-01-18 12:45:45 -0500

    Created components for success, loading, failure

---
+++ client/src/components/Loader.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import system from '../design/theme'
+import rings from '../img/rings.svg'
+
+const Loader = () => {
+  return (
+    // show this component when your component is loading, request is pending, etc.
+    <Container>
+      <img src={rings} />
+      <p>
+        We'll get you squared away in just a moment. Thanks for your patience.
+      </p>
+    </Container>
+  )
+}
+
+export default Loader
+
+const Container = styled.div`
+  display: flex;
+  position: relative;
+  flex-flow: row nowrap;
+  align-items: center;
+  width: 60%;
+  background: ${system.color.bodytext};
+  padding: ${system.spacing.standardPadding};
+  border-radius: ${system.borders.bigRadius};
+  box-shadow: ${system.shadows.otherLight};
+  margin-bottom: 20px;
+
+  p {
+    color: ${system.color.white};
+    margin-left: ${system.spacing.bigPadding};
+    font-size: ${system.fontSizing.m};
+  }
+
+  img {
+    height: 50px;
+    width: 50px;
+  }
+`
