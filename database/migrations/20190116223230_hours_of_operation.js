a8f7d1acc50659499af5b7121f48a3cc84d07c5f
adamhinckley <adamhinckley@mac.com>
2019-01-18 08:59:02 -0700

    adds notNullable() to hours of operation db

--- database/migrations/20190116223230_hours_of_operation.js
+++ database/migrations/20190116223230_hours_of_operation.js
@@ -2,9 +2,9 @@
 exports.up = knex =>
   knex.schema.createTable('hours_of_operation', table => {
     table.uuid('id').primary()
-    table.float('day') //should be notNullable but the seed is missing this for now
-    table.float('open_time') //should be notNullable but the seed is missing this for now
-    table.float('close_time') //should be notNullable but the seed is missing this for now
+    table.float('day').notNullable()
+    table.float('open_time').notNullable()
+    table.float('close_time').notNullable()
     table.boolean('closed')
     table
       .uuid('organization_id')
