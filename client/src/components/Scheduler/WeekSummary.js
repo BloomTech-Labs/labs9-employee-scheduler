2e02035533fe9a7684a935eeb8f1cf683e0cd8b2
Rahul Desai <rd825@georgetown.edu>
2019-01-21 17:54:54 -0500

    Fixed summaries component

--- client/src/components/Scheduler/WeekSummary.js
+++ client/src/components/Scheduler/WeekSummary.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import DailySummary from './DailySummary'
 import styled from '@emotion/styled'
-import system from '../../design/theme'
-
-function sortFunction(a, b) {
-  var dateA = new Date(a.day).getTime()
-  var dateB = new Date(b.day).getTime()
-  return dateA > dateB ? 1 : -1
-}
+import moment from 'moment'
 
 Array.prototype.unique = function() {
   return this.filter(function(value, index, self) {
@@ -15,57 +9,100 @@ Array.prototype.unique = function() {
   })
 }
 
-export default function WeekSummary(props) {
-  const { events } = props
-  const eventsArr = []
-  events.map(event => {
-    event = {
-      employee: event.title,
-      day: event.start.toLocaleDateString('en-US', 'numeric'),
-      hours: Math.abs(event.end - event.start) / 3600000
-    }
-    eventsArr.push(event)
-  })
-  const shifts = eventsArr.sort(sortFunction)
+class WeekSummary extends React.Component {
+  componentDidMount = () => {}
 
-  const reduced = shifts.reduce((acc, current) => {
-    const { employee, day, hours } = current
-    const newItem = { employees: [employee], hours: hours }
-
-    if (acc[day]) {
-      acc[day] = {
-        ...acc[day],
-        day: acc[day],
-        hours: acc[day].hours + newItem.hours,
-        employees: acc[day].employees.concat(newItem.employees)
-      }
-    } else {
-      acc[day] = newItem
-    }
-
-    return acc
-  }, {})
-
-  const days = Object.keys(reduced)
-  const counts = Object.values(reduced)
-  const summaries = []
-  for (let i = 0; i < days.length; i++) {
-    summaries[i] = {
-      day: days[i],
-      employees: counts[i].employees.unique().length,
-      hours: counts[i].hours
-    }
+  sortFunction = (a, b) => {
+    var dateA = new Date(a.day).getTime()
+    var dateB = new Date(b.day).getTime()
+    return dateA > dateB ? 1 : -1
   }
 
+  rangeFilter = (events, range) => {
+    console.log(range)
+    let filtered = null
+    if (Array.isArray(range) && range.length === 1) {
+      filtered = events.filter(
+        event =>
+          event.start >= moment(range[0]).startOf('day') &&
+          event.end <= moment(range[0]).endOf('day')
+      )
+    } else if (Array.isArray(range)) {
+      filtered = events.filter(
+        event =>
+          event.start >= moment(range[0]).startOf('day') &&
+          event.end <= moment(range[6]).endOf('day')
+      )
+    } else {
+      console.log(events)
+      filtered = events.filter(
+        event => event.start >= range.start && event.end <= range.end
+      )
+    }
+    return filtered
+  }
 
-  return (
-    <Div>
-      {summaries.map((summary, i) => (
-        <DailySummary key={i} summary={summary} />
-      ))}
-    </Div>
-  )
+  summarize = filtered => {
+    const eventsArr = []
+    const summaries = []
+    filtered.map(event => {
+      event = {
+        employee: event.title,
+        day: event.start.toLocaleDateString('en-US', 'numeric'),
+        hours: Math.abs(event.end - event.start) / 3600000
+      }
+      eventsArr.push(event)
+    })
+    const shifts = eventsArr.sort(this.sortFunction)
+
+    const reduced = shifts.reduce((acc, current) => {
+      const { employee, day, hours } = current
+      const newItem = { employees: [employee], hours: hours }
+
+      if (acc[day]) {
+        acc[day] = {
+          ...acc[day],
+          day: acc[day],
+          hours: acc[day].hours + newItem.hours,
+          employees: acc[day].employees.concat(newItem.employees)
+        }
+      } else {
+        acc[day] = newItem
+      }
+
+      return acc
+    }, {})
+
+    const days = Object.keys(reduced)
+    const counts = Object.values(reduced)
+    for (let i = 0; i < days.length; i++) {
+      summaries[i] = {
+        day: days[i],
+        employees: counts[i].employees.unique().length,
+        hours: counts[i].hours
+      }
+    }
+    return summaries
+  }
+
+  render() {
+    const { events, range } = this.props
+    const filtered = this.rangeFilter(events, range)
+    const summaries = this.summarize(filtered)
+    console.log(summaries)
+    return (
+      <Div>
+        {summaries
+          ? summaries.map((summary, i) => (
+              <DailySummary key={i} summary={summary} />
+            ))
+          : null}
+      </Div>
+    )
+  }
 }
+
+export default WeekSummary
 
 const Div = styled.div`
   display: flex;
