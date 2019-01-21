982bc05470601ad759f2d6e1b5a7f80bd3804c4f
Axolotl <samuelmachat@gmail.com>
2019-01-21 14:09:44 -0500

    create new user via invite endpoint working

--- server/routers/invitesRouter.js
+++ server/routers/invitesRouter.js
@@ -49,36 +49,34 @@ router.post(
   invite('employee')
 )
 
-router.post(
-  '/register/:id',
-  authorize(['owner', 'supervisor']),
-  async (req, res) => {
-    const { id } = req.params // invite id
-    const { email, phone, firstName, lastName } = req.body // user info
-    const { organization_id, role } = getInvite(id) // grab info from the invite
+// no need to authorize this route because it's coming in from a new person
+// so by definition it's coming in from someone who is not yet authorized
+router.post('/register/:id', async (req, res) => {
+  const { id } = req.params // invite id
+  const { email, phone, firstName, lastName } = req.body // user info
+  const { organization_id, role } = await getInvite(id) // grab info from the invite
 
-    if (!email || !name || !firstName || !lastName) {
-      res.status(400).json({ error: 'Missing required field(s)' })
-    }
-
-    const newUser = {
-      organization_id,
-      first_name: firstName,
-      last_name: lastName,
-      role,
-      email,
-      phone
-    }
-
-    const success = await addUser(newUser)
-
-    if (success) {
-      res.status(201).json({ message: 'Success' })
-    } else {
-      res.status(500).json({ error: 'Error' })
-    }
+  if (!email || !phone || !firstName || !lastName) {
+    res.status(400).json({ error: 'Missing required field(s)' })
   }
-)
+
+  const newUser = {
+    organization_id,
+    first_name: firstName,
+    last_name: lastName,
+    role,
+    email,
+    phone
+  }
+
+  const success = await addUser(newUser)
+
+  if (success) {
+    res.status(201).json({ message: 'Success' })
+  } else {
+    res.status(500).json({ error: 'Error' })
+  }
+})
 
 router.post('/send-test', (req, res) => {
   console.log('sending email to test account')
