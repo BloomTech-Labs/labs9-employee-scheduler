f0ce1fca2120fb2f378888d727cfe5f6cabd6e41
Axolotl <samuelmachat@gmail.com>
2019-01-22 13:09:34 -0500

    join component working

--- server/routers/invitesRouter.js
+++ server/routers/invitesRouter.js
@@ -51,16 +51,18 @@ router.post(
 
 // no need to authorize this route because it's coming in from a new person
 // so by definition it's coming in from someone who is not yet authorized
-router.post('/register/:id', async (req, res) => {
-  const { id } = req.params // invite id
+router.post('/register/:inviteId', async (req, res) => {
+  const { inviteId } = req.params // invite id
+  const { id } = req.user // pulled from firebase token
   const { email, phone, firstName, lastName } = req.body // user info
-  const { organization_id, role } = await getInvite(id) // grab info from the invite
+  const { organization_id, role } = await getInvite(inviteId) // grab info from the invite
 
   if (!email || !firstName || !lastName) {
     res.status(400).json({ error: 'Missing required field(s)' })
   }
 
   const newUser = {
+    id,
     organization_id,
     first_name: firstName,
     last_name: lastName,
