3783fee8f9e6a424590d0695a957bbdab23d1035
Carlos Lantigua <clantigua954@gmail.com>
2019-01-23 17:57:47 -0500

    finished redux action

--- client/src/reducers/employeeReducer.js
+++ client/src/reducers/employeeReducer.js
@@ -1,6 +1,8 @@
 import {
   FETCH_EMPLOYEE_FROM_DB_SUCCESS,
-  FETCH_EMPLOYEE_FROM_DB_FAIL
+  FETCH_EMPLOYEE_FROM_DB_FAIL,
+  DELETE_TIME_OFF_REQUEST_SUCCESS,
+  DELETE_TIME_OFF_REQUEST_FAILED
 } from '../actions'
 
 const initialState = {
@@ -17,6 +19,16 @@ export const employeeReducer = (state = initialState, action) => {
       }
     case FETCH_EMPLOYEE_FROM_DB_FAIL:
       return { ...initialState, error: 'fetching failed' }
+    case DELETE_TIME_OFF_REQUEST_SUCCESS:
+      return {
+        employee: action.payload,
+        error: ''
+      }
+    case DELETE_TIME_OFF_REQUEST_FAILED:
+      return {
+        employee: action.payload,
+        error: ''
+      }
     default:
       return state
   }
