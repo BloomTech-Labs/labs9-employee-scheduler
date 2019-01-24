e2333b82dace87cf69613ec38d45e6577ba02adb
Axolotl <samuelmachat@gmail.com>
2019-01-24 12:13:08 -0500

    create action and reducer for coverage

---
+++ client/src/actions/coverageActions.js
@@ -0,0 +1,8 @@
+const axios = require('axios')
+
+export const DISPLAY_COVERAGE = 'DISPLAY_COVERAGE'
+
+export const displayCoverage = coverage => ({
+  type: DISPLAY_COVERAGE,
+  payload: coverage
+})
