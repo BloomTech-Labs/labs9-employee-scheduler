a3a4acd1ac23801bf4a75b860059e8b535d0215f
Kamry Bowman <kamry.bowman@gmail.com>
2019-01-28 18:31:10 -0700

    fix PTO issue

--- server/routers/timeOffRequestsRouter.js
+++ server/routers/timeOffRequestsRouter.js
@@ -63,10 +63,11 @@ router.put('/:id', authorize(['owner', 'supervisor']), (req, res) => {
 router.delete('/:id', authorize(['owner', 'supervisor']), (req, res) => {
   const { id } = req.params
   deleteTimeOffRequest(id)
-    .then(res => res.status(200).json(res))
-    .catch(err =>
-      res.status(404).json({ error: 'Error deleting request', err })
-    )
+    .then(success => res.status(200).json(success))
+    .catch(err => {
+      console.log(err)
+      return res.status(404).json({ error: 'Error deleting request', err })
+    })
 })
 
 // router.get('/', authorize(['owner', 'supervisor']), (req, res) => {
