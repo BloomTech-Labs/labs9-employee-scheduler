107adb3e59a421e5635d1ae8cb4384d293ab2309
Axolotl <samuelmachat@gmail.com>
2019-02-01 14:17:03 -0500

    delete rest of deprecated tests

--- __tests__/organizationsRouter.js
+++ __tests__/organizationsRouter.js
@@ -7,25 +7,6 @@ const { generateTeamData } = require('../database/utils/generateData')
 const request = supertest(server)
 
 describe('testing the organizations router', () => {
-  describe('GET', () => {
-    it('should return all organizations - GET /', async () => {
-      const expected = await knex('organizations')
-      const response = await request
-        .get('/organizations')
-        .set('authorization', 'testing')
-
-      expect(response.status).toEqual(200)
-      expect(response.body.length).toEqual(expected.length)
-    })
-
-    it('should return a JSON response', async () => {
-      const response = await request
-        .get('/organizations')
-        .set('authorization', 'testing')
-      expect(response.type).toEqual('application/json')
-    })
-  })
-
   describe('GET /:id', () => {
     it('returns the correct organization by ID - GET /:id', async () => {
       const { team, cleanup } = await generateTeamData(knex)
