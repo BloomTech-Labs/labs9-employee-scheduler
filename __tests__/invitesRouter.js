b59dc2951d281e0bb6173b3e6f300236aa20e56d
Axolotl <samuelmachat@gmail.com>
2019-02-04 16:45:22 -0500

    tests for post /invite-supervisor

--- __tests__/invitesRouter.js
+++ __tests__/invitesRouter.js
@@ -7,33 +7,47 @@ const {} = require('../database/utils/generateData')
 const request = supertest(server)
 
 describe('testing the invites router', () => {
+  // both of the POST routes depend on the testing middleware
+  // instad of needing pass a token to get the user id,
+  // the testin middleware allows you to specify user type
+  // without never needing to worry about generating a token
   describe('POST /invite-supervisor', () => {
     it('posts with correct auth and body', async () => {
-      // logic
-      // sample logic follows:
-      // const { team, cleanup } = await generateTeamData(knex)
-      // const { id } = team.organization
+      const response = await request
+        .post('/invites/invite-supervisor')
+        .send({
+          name: 'Testing McTestyFace',
+          email: 'test@testymctestyface.com'
+        })
+        .set('authorization', 'testing')
+        .set('user', 'owner')
 
-      // const expected = await knex('organizations').where({
-      //   id: id
-      // })
-
-      // const response = await request
-      //   .get(`/organizations/${id}`)
-      //   .set('authorization', 'testing')
-
-      // expect(response.status).toEqual(200)
-      // expect([response.body]).toEqual(expected)
-
-      // await cleanup()
+      expect(response.status).toEqual(201)
     })
 
     it('fails with incorrect auth', async () => {
-      // logic
+      const response = await request
+        .post('/invites/invite-supervisor')
+        .send({
+          name: 'Testing McTestyFace',
+          email: 'test@testymctestyface.com'
+        })
+        .set('authorization', 'testing')
+        .set('user', 'supervisor')
+
+      expect(response.status).toEqual(403)
     })
 
     it('fails with incorrect body', async () => {
-      // logic
+      const response = await request
+        .post('/invites/invite-supervisor')
+        .send({
+          name: 'Testing McTestyFace'
+        })
+        .set('authorization', 'testing')
+        .set('user', 'owner')
+
+      expect(response.status).toEqual(400)
     })
   })
 
@@ -62,18 +76,6 @@ describe('testing the invites router', () => {
 
     it('fails with incorrect id', async () => {
       // logic
-    })it('posts with correct auth and body', async () => {
-      // logic
-    })
-
-    it('fails with incorrect auth', async () => {
-      // logic
-    })
-
-    it('fails with incorrect body', async () => {
-      // logic
     })
   })
 })
-
-
