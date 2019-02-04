66f8737fb011230c9c8ee9cf55050054638c6639
Carlos Lantigua <clantigua954@gmail.com>
2019-02-03 19:15:19 -0500

    completed put request

--- __tests__/timeOffRequestsRouter.js
+++ __tests__/timeOffRequestsRouter.js
@@ -52,22 +52,34 @@ describe('PUT /:id', () => {
   it('responds with 200 when a new put request goes through', async () => {
     const { team, cleanup } = await generateTeamData(knex)
 
-    const { id } = await team.users[0]
-    const expected = await knex('time_off_requests')
-      .where({ user_id: id })
-      .first()
-
+    // const { id } = await team.users[0]
+    const { users } = team
+    // need to add cal_visit and emp_visit because they are not added by
+    // generateTeamData but instead are default values in the db model
+    const target = { ...users[0], cal_visit: true, emp_visit: true }
     const changes = {
-      start: '2019-02-20 00:00:00:-05',
-      end: '2019-02-25 23:59:59-05'
+      start: '2019-02-20 01:00:00-05',
+      end: '2019-02-20 23:59:59-05'
     }
     const response = await request
-      .put(`/time-off-requests/${id}`)
+      .put(`/time-off-requests/${target.id}`)
       .send(changes)
       .set('authorization', 'testing')
 
-    expect(response.status).toEqual(200)
+    let updatedUser = await knex('users')
+      .where({ id: target.id })
+      .first()
 
+    updatedUser = {
+      ...updatedUser,
+      start: changes.start,
+      end: changes.end
+    }
+
+    expect(response.status).toEqual(200)
+    expect(response.type).toEqual('application/json')
+    expect(response.body).toEqual('')
+    expect(updatedUser).toEqual({ ...target, ...changes })
     await cleanup()
   })
 })
