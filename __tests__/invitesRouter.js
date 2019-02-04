d61cdf5bc76208d02d3919cbcde451b7d7782c80
Axolotl <samuelmachat@gmail.com>
2019-02-04 18:25:50 -0500

    get all tests working for creating invites

--- __tests__/invitesRouter.js
+++ __tests__/invitesRouter.js
@@ -1,8 +1,8 @@
 const supertest = require('supertest')
 const server = require('../server/server')
-const knex = require('../database/dbConfig')
-const uuid = require('uuid/v4')
-const {} = require('../database/utils/generateData')
+// const knex = require('../database/dbConfig')
+// const uuid = require('uuid/v4')
+// const {} = require('../database/utils/generateData')
 
 const request = supertest(server)
 
@@ -11,6 +11,34 @@ describe('testing the invites router', () => {
   // instad of needing pass a token to get the user id,
   // the testin middleware allows you to specify user type
   // without never needing to worry about generating a token
+  describe('POST /invite-employee', () => {
+    it('posts with correct auth and body', async () => {
+      const response = await request
+        .post('/invites/invite-employee')
+        .send({
+          name: 'Testing McTestyFace',
+          email: 'test@testymctestyface.com'
+        })
+        .set('authorization', 'testing')
+        .set('user', 'supervisor')
+
+      expect(response.status).toEqual(201)
+    })
+
+    it('fails with incorrect auth', async () => {
+      const response = await request
+        .post('/invites/invite-employee')
+        .send({
+          name: 'Testing McTestyFace',
+          email: 'test@testymctestyface.com'
+        })
+        .set('authorization', 'testing')
+        .set('user', 'employee')
+
+      expect(response.status).toEqual(403)
+    })
+  })
+
   describe('POST /invite-supervisor', () => {
     it('posts with correct auth and body', async () => {
       const response = await request
@@ -42,26 +70,13 @@ describe('testing the invites router', () => {
       const response = await request
         .post('/invites/invite-supervisor')
         .send({
-          name: 'Testing McTestyFace'
+          name: 'Testing McTestyFace',
+          email2: 'test@testymctestyface.com'
         })
         .set('authorization', 'testing')
         .set('user', 'owner')
 
       expect(response.status).toEqual(400)
-    })
-  })
-
-  describe('POST /invite-employee', () => {
-    it('posts with correct auth and body', async () => {
-      // logic
-    })
-
-    it('fails with incorrect auth', async () => {
-      // logic
-    })
-
-    it('fails with incorrect body', async () => {
-      // logic
     })
   })
 
