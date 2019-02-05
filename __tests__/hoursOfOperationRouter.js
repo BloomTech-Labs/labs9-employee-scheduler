ae7b26cdb5ad957757962afa4a82c6ba19f74cc2
Axolotl <samuelmachat@gmail.com>
2019-02-05 18:56:46 -0500

    add second hours of op test

--- __tests__/hoursOfOperationRouter.js
+++ __tests__/hoursOfOperationRouter.js
@@ -9,7 +9,7 @@ describe('testing the hours of operation router ', () => {
 
     const response = await request
       .get(`/hours-of-operation/${orgId}`)
-      .set('authorization', 'token')
+      .set('authorization', 'testing')
       .set('user', 'owner')
 
     const numberOfDays = response.body.length
@@ -26,5 +26,36 @@ describe('testing the hours of operation router ', () => {
 
     expect(numberOfDays).toBe(7)
     expect(oneOfEachDay).toBe(true)
+  })
+
+  it('puts a change to hours of operation', async () => {
+    const orgId = '9126df31-2607-4166-9c0c-d0a300c59c62'
+
+    const response = await request
+      .get(`/hours-of-operation/${orgId}`)
+      .set('authorization', 'testing')
+      .set('user', 'owner')
+
+    const dayToAlter = response.body.find(({ day }) => day === 0)
+
+    const alteredDay = {
+      ...dayToAlter,
+      open_time: dayToAlter.open_time === '16:00:00' ? '15:00:00' : '16:00:00'
+    }
+
+    await request
+      .put(`/hours-of-operation/${dayToAlter.id}`)
+      .send(alteredDay)
+      .set('authorization', 'testing')
+      .set('user', 'owner')
+
+    const updated = await request
+      .get(`/hours-of-operation/${orgId}`)
+      .set('authorization', 'testing')
+      .set('user', 'owner')
+
+    const updatedDay = updated.body.find(({ day }) => day === 0)
+
+    expect(updatedDay).toEqual(alteredDay)
   })
 })
