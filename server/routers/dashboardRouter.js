14be5ca49dab7ed5a4d86240252dc42354ed27ca
Axolotl <samuelmachat@gmail.com>
2019-02-05 16:41:44 -0500

    configure dashboard router to handle errors

--- server/routers/dashboardRouter.js
+++ server/routers/dashboardRouter.js
@@ -5,11 +5,15 @@ const { getDashboard } = require('../../database/helpers')
 const authorize = require('../config/customMiddleware/authorize')
 
 router.get('/', authorize(['all']), (req, res) => {
-  const { id } = req.user
-  // id is user id
-  getDashboard(id)
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(404).json(err))
+  try {
+    const { id } = req.user
+    // id is user id
+    getDashboard(id)
+      .then(users => res.status(200).json(users))
+      .catch(err => res.status(404).json(err))
+  } catch (error) {
+    res.status(404).json(err)
+  }
 })
 
 module.exports = router
