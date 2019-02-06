6e3abde6b5ba553c081dac0e468d0230f87fa16a
Axolotl <samuelmachat@gmail.com>
2019-02-06 17:57:06 -0500

    structure basics of test

--- __tests__/paymentRouter.js
+++ __tests__/paymentRouter.js
@@ -1,5 +1,18 @@
-describe('works', () => {
-  it('true is true', async () => {
+const supertest = require('supertest')
+const server = require('../server/server')
+const db = require('../database/dbConfig')
+const uuid = require('uuid/v4')
+
+const request = supertest(server)
+
+const orgId = '9126df31-2607-4166-9c0c-d0a300c59c62' // cadence id
+
+describe('testing payments router', () => {
+  it('posts payment with a valid token', async () => {
+    expect(true)
+  })
+
+  it('puts a change to the subscription', async () => {
     expect(true)
   })
 })
