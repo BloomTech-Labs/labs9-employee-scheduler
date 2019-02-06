2c515f1d62763daadc4adb1d8540230f0964fa54
Kamry Bowman <kamry.bowman@gmail.com>
2019-02-06 16:57:02 -0700

    attempt more explicit cleanup for travis ci troubleshoot

--- client/src/__tests__/Login.js
+++ client/src/__tests__/Login.js
@@ -4,7 +4,12 @@ import {
   populateOrg,
   structureEmployees
 } from '../../../database/utils/generateData'
-import { fireEvent, waitForElement, getByText } from 'react-testing-library'
+import {
+  fireEvent,
+  waitForElement,
+  getByText,
+  cleanup
+} from 'react-testing-library'
 import { renderWithReduxAndRouter, setupStripeNode } from '../../testing/utils'
 
 import * as axios from 'axios'
@@ -46,48 +51,49 @@ const user = users.find(user => user.role === 'owner')
 user.cal_visit = false
 
 describe('Login Component', () => {
-  // it('should navigate to login for non-logged in user', async () => {
-  //   setupStripeNode()
-  //   // mock out axios authenticaton call to our server
-  //   axios.get.mockImplementation((path, { headers: { authorization } }) => {
-  //     if (authorization === 'token') {
-  //       if (path.match(new RegExp(`/organizations/${organization.id}`))) {
-  //         return Promise.resolve({ data: organization })
-  //       }
-  //       if (path.match(new RegExp(`/users/current`))) {
-  //         return Promise.resolve({ data: user })
-  //       }
-  //     }
-  //   })
-  //   // mock out firebase auth
-  //   firebase.auth = jest.fn().mockImplementation(() => {
-  //     return {
-  //       onAuthStateChanged: cb => {
-  //         cb()
-  //         return () => {}
-  //       },
-  //       currentUser: {
-  //         getIdToken: () => Promise.reject({ status: 404 })
-  //       }
-  //     }
-  //   })
+  afterEach(cleanup)
+  it('should navigate to login for non-logged in user', async () => {
+    setupStripeNode()
+    // mock out axios authenticaton call to our server
+    axios.get.mockImplementation((path, { headers: { authorization } }) => {
+      if (authorization === 'token') {
+        if (path.match(new RegExp(`/organizations/${organization.id}`))) {
+          return Promise.resolve({ data: organization })
+        }
+        if (path.match(new RegExp(`/users/current`))) {
+          return Promise.resolve({ data: user })
+        }
+      }
+    })
+    // mock out firebase auth
+    firebase.auth = jest.fn().mockImplementation(() => {
+      return {
+        onAuthStateChanged: cb => {
+          cb()
+          return () => {}
+        },
+        currentUser: {
+          getIdToken: () => Promise.reject({ status: 404 })
+        }
+      }
+    })
 
-  //   firebase.auth.GoogleAuthProvider = {
-  //     PROVIDER_ID: '1'
-  //   }
-  //   firebase.auth.EmailAuthProvider = {
-  //     PROVIDER_ID: '1'
-  //   }
+    firebase.auth.GoogleAuthProvider = {
+      PROVIDER_ID: '1'
+    }
+    firebase.auth.EmailAuthProvider = {
+      PROVIDER_ID: '1'
+    }
 
-  //   // renders the App with both Redux and Router, with the route set
-  //   // to the matching route for this component in App
-  //   const { queryByText } = await renderWithReduxAndRouter(<App />, {
-  //     route: `/login`
-  //   })
+    // renders the App with both Redux and Router, with the route set
+    // to the matching route for this component in App
+    const { queryByText } = await renderWithReduxAndRouter(<App />, {
+      route: `/login`
+    })
 
-  //   const loginButton = await waitForElement(() => queryByText(/sign in/i))
-  //   expect(loginButton).not.toBeNull()
-  // })
+    const loginButton = await waitForElement(() => queryByText(/sign in/i))
+    expect(loginButton).not.toBeNull()
+  })
 
   it('should redirect away from Login for logged in user', async () => {
     setupStripeNode()
