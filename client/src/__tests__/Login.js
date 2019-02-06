7a317dbf02caf6ca5a6af6231dcebc62306fc0c5
Kamry Bowman <kamry.bowman@gmail.com>
2019-02-06 12:48:28 -0700

    implement Login component test

--- client/src/__tests__/Login.js
+++ client/src/__tests__/Login.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import App from '../App'
-import { populateOrg } from '../../../database/utils/generateData'
+import {
+  populateOrg,
+  structureEmployees
+} from '../../../database/utils/generateData'
 import { fireEvent, waitForElement, getByText } from 'react-testing-library'
 import { renderWithReduxAndRouter, setupStripeNode } from '../../testing/utils'
 
@@ -13,30 +16,52 @@ jest.mock('firebase/auth')
 jest.mock('react-ga')
 
 jest.mock('react-firebaseui/StyledFirebaseAuth', () => {
-  const FirebaseUI = jest.fn(({ uiConfig }) => {
+  const FirebaseUI = jest.fn(props => {
+    const { uiConfig, firebaseAuth } = props
     const validUIConfig =
       uiConfig &&
-      uiConfig.signOptions &&
-      uiConfig.signOptions.every(option => option !== undefined)
-    if (!validUIConfig) {
+      uiConfig.signInOptions &&
+      uiConfig.signInOptions.every(option => option !== undefined)
+    if (!validUIConfig || !firebaseAuth) {
       throw new Error('bad firebase config')
     }
 
     return (
       <div>
-        <button>Sign in with email</button>
+        <button>Sign in</button>
       </div>
     )
   })
   return FirebaseUI
 })
 
-const { organization, users } = populateOrg({ size: 6 })
-const user = users[0]
+// generate org with table structure
+const org = populateOrg({ size: 4 })
+
+// convert into nested employee data structure
+const employees = structureEmployees(org)
+const { users, events, hours, organization } = org
+const user = users.find(user => user.role === 'owner')
+user.cal_visit = false
 
 describe('Login Component', () => {
   let email, phone, emailpref, phonepref, utils
-  beforeEach(() => {})
+  beforeEach(() => {
+    // setup of document to play nice with Striple component
+    setupStripeNode()
+
+    // mock out axios authenticaton call to our server
+    axios.get.mockImplementation((path, { headers: { authorization } }) => {
+      if (authorization === 'token') {
+        if (path.match(new RegExp(`/organizations/${organization.id}`))) {
+          return Promise.resolve({ data: organization })
+        }
+        if (path.match(new RegExp(`/users/current`))) {
+          return Promise.resolve({ data: user })
+        }
+      }
+    })
+  })
 
   it('should navigate to login for non-logged in user', async () => {
     // mock out firebase auth
@@ -59,10 +84,48 @@ describe('Login Component', () => {
       PROVIDER_ID: '1'
     }
 
-    // mock out axios authenticaton call to our server
+    // renders the App with both Redux and Router, with the route set
+    // to the matching route for this component in App
+    const { queryByText } = await renderWithReduxAndRouter(<App />, {
+      route: `/login`
+    })
+
+    const loginButton = await waitForElement(() => queryByText(/sign in/i))
+    expect(loginButton).not.toBeNull()
+  })
+
+  it('should redirect away from Login for logged in user', async () => {
+    // mock out firebase auth
+    firebase.auth = jest.fn().mockImplementation(() => {
+      return {
+        onAuthStateChanged: cb => {
+          cb()
+          return () => {}
+        },
+        currentUser: {
+          getIdToken: () => Promise.resolve('token')
+        }
+      }
+    })
+
+    firebase.auth.GoogleAuthProvider = {
+      PROVIDER_ID: '1'
+    }
+    firebase.auth.EmailAuthProvider = {
+      PROVIDER_ID: '1'
+    }
+
     axios.get.mockImplementation((path, { headers: { authorization } }) => {
       if (authorization === 'token') {
-        if (path.match(new RegExp(`/organizations/${organization.id}`))) {
+        if (path.match(new RegExp(`/employees/${user.organization_id}`))) {
+          return Promise.resolve({ data: employees })
+        }
+        if (
+          path.match(new RegExp(`/hours-of-operation/${user.organization_id}`))
+        ) {
+          return Promise.resolve({ data: hours })
+        }
+        if (path.match(new RegExp(`/organizations/${user.organization_id}`))) {
           return Promise.resolve({ data: organization })
         }
         if (path.match(new RegExp(`/users/current`))) {
@@ -70,24 +133,20 @@ describe('Login Component', () => {
         }
       }
     })
-    // setup of document to play nice with Striple component
-    setupStripeNode()
 
     // renders the App with both Redux and Router, with the route set
     // to the matching route for this component in App
-    const { queryByText } = await renderWithReduxAndRouter(<App />, {
+    const { queryByText, container } = await renderWithReduxAndRouter(<App />, {
       route: `/login`
     })
 
-    const loginComp = await waitForElement(() => queryByText(/sign on/i))
-    expect(loginComp).not.toBeNull()
-  })
+    const loginButton = await waitForElement(() => queryByText(/sign in/i))
+    expect(loginButton).not.toBeNull()
 
-  // it('should take a phonepref number values to state', async () => {
-  //   const phonepref = await waitForElement(() => {
-  //     return utils.getByLabelText(/phonepref/i)
-  //   })
-  //   fireEvent.change(phonepref, { target: { value: '999999999' } })
-  //   expect(phonepref.value).toBe('999999999')
-  // })
+    const cal = await waitForElement(() =>
+      container.querySelector('.rbc-calendar')
+    )
+    expect(cal).not.toBeNull()
+    expect(queryByText(/sign in/i)).toBeNull()
+  })
 })
