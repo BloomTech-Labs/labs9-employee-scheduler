b07767cf8f5680cea22bef4ea01a4fe706efe450
Axolotl <samuelmachat@gmail.com>
2019-02-06 19:47:31 -0500

    clean up code

--- server/routers/paymentRouter.js
+++ server/routers/paymentRouter.js
@@ -65,9 +65,7 @@ router.put('/', authorize(['owner']), (req, res) => {
         customer_id: null,
         paid: false
       })
-        .then(() => {
-          return res.status(200).json({ message: 'Subscription cancelled' })
-        })
+        .then(() => res.status(200).json({ message: 'Subscription cancelled' }))
         .catch(err => res.status(500).json({ message: 'Server error', err }))
     }
   })
