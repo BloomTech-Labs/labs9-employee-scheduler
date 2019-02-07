9ca1f07f3c74ab5169f1f7e95d5d37532f42ab26
Kamry Bowman <kamry.bowman@gmail.com>
2019-02-07 13:31:37 -0700

    add clusters to servers

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
 require('dotenv').config()
+const cluster = require('cluster')
+const os = require('os')
 const server = require('./server.js')
 const port = process.env.PORT || 9000
 
-server.listen(port, () => console.log(`\n** server up on port ${port} **\n`))
+if (cluster.isMaster) {
+  const cpuCount = os.cpus().length
+  for (let i = 0; i < cpuCount; i++) {
+    console.log('spawning child cluster')
+    cluster.fork()
+  }
+} else {
+  server.listen(port, () => console.log(`\n** server up on port ${port} **\n`))
+}
+
+cluster.on('exit', worker => {
+  console.log('worker exiting')
+  cluster.fork()
+})
